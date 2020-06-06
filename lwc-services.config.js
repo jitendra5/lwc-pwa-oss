c0dd8f7c65ddfd91fd0cf97a1fdafe207320e64f
Jithendra <jithendra@Jithendras-MacBook-Pro.local>
2020-06-06 19:39:26 +0530

    added dependency libraries

--- lwc-services.config.js
+++ lwc-services.config.js
@@ -5,17 +5,17 @@ module.exports = {
         { from: 'src/resources', to: 'dist/resources/' },
         { from: 'src/index.html', to: 'dist/' },
         { from: 'src/manifest.json', to: 'dist/' },
-        {
-            from: 'node_modules/jquery/dist/jquery.min.js',
-            to: 'src/bootstrap/js'
-        },
-        {
-            from: 'node_modules/jquery/dist/jquery.min.js',
-            to: 'dist/bootstrap/js'
-        },
-        { from: 'node_modules/bootstrap/dist/css', to: 'src/bootstrap/css' },
-        { from: 'node_modules/bootstrap/dist/css', to: 'dist/bootstrap/css' },
-        { from: 'node_modules/bootstrap/dist/js', to: 'src/bootstrap/js' },
-        { from: 'node_modules/bootstrap/dist/js', to: 'dist/bootstrap/js' }
+        // {
+        //     from: 'node_modules/jquery/dist/jquery.min.js',
+        //     to: 'src/bootstrap/js'
+        // },
+        // {
+        //     from: 'node_modules/jquery/dist/jquery.min.js',
+        //     to: 'dist/bootstrap/js'
+        // },
+        { from: 'node_modules/bootstrap/dist/css/bootstrap.min.css', to: 'src/bootstrap/css' },
+        { from: 'node_modules/bootstrap/dist/css/bootstrap.min.css', to: 'dist/bootstrap/css' },
+        // { from: 'node_modules/bootstrap/dist/js', to: 'src/bootstrap/js' },
+        // { from: 'node_modules/bootstrap/dist/js', to: 'dist/bootstrap/js' }
     ]
 };
--- scripts/webpack.config.js
+++ scripts/webpack.config.js
@@ -1,7 +1,20 @@
 // Custom webpack configuration file, provides generation of service worker
 // More information: https://developers.google.com/web/tools/workbox/modules/workbox-webpack-plugin
 const { GenerateSW } = require('workbox-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    plugins: [new GenerateSW({ swDest: 'sw.js' })]
+    plugins: [
+        new CopyPlugin([
+            './src/index.html',
+            './src/index.js',
+            './src/manifest.json',
+            './src'
+        ]),
+        new GenerateSW({ 
+            swDest: 'sw.js',
+            clientsClaim: true,
+            // skipWaiting: true
+         })
+    ]
 };
